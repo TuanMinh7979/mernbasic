4697a9a665ca9225eada3dd0cebaf58fce8c3fb4
TuanMinh7979 <minhtuanto179@gmail.com>
2022-08-16 08:20:04 +0700

    design main page

--- trelloreact/src/App.js
+++ trelloreact/src/App.js
@@ -1,8 +1,99 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="trello-main-app">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header> Brainstorm</header>
+          <ul>
+            <li>
+               <img
+                src="https://imgs.search.brave.com/UwqyTvQWl82NmIKWCMUsQYa9xGcYrWizMJeFlvH-PcU/rs:fit:770:514:1/g:ce/aHR0cHM6Ly93d3cu/ZmxpcHBpbmdoZWNr/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNy9ob3ct/dG8tZ2V0LW1vdGl2/YXRlZC5qcGc"
+                alt=""
+              ></img> 
+              Title: TRUNGQUANDEV
+            </li>
+            <li>abc</li>
+            <li>abc</li>
+            <li>abc</li>
+          </ul>
+
+          <footer>Footer</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm</header>
+          <ul>
+            <li>
+               <img
+                src="https://imgs.search.brave.com/UwqyTvQWl82NmIKWCMUsQYa9xGcYrWizMJeFlvH-PcU/rs:fit:770:514:1/g:ce/aHR0cHM6Ly93d3cu/ZmxpcHBpbmdoZWNr/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNy9ob3ct/dG8tZ2V0LW1vdGl2/YXRlZC5qcGc"
+                alt=""
+              ></img> 
+              Title: TRUNGQUANDEV
+            </li>
+            <li>abc</li>
+            <li>abc</li>
+            <li>abc</li>
+          </ul>
+
+          <footer>Footer</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm</header>
+          <ul>
+            <li>
+               <img
+                src="https://imgs.search.brave.com/UwqyTvQWl82NmIKWCMUsQYa9xGcYrWizMJeFlvH-PcU/rs:fit:770:514:1/g:ce/aHR0cHM6Ly93d3cu/ZmxpcHBpbmdoZWNr/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNy9ob3ct/dG8tZ2V0LW1vdGl2/YXRlZC5qcGc"
+                alt=""
+              ></img> 
+              Title: TRUNGQUANDEV
+            </li>
+            <li>abc</li>
+            <li>abc</li>
+            <li>abc</li>
+          </ul>
+
+          <footer>Footer</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm</header>
+          <ul>
+            <li>
+               <img
+                src="https://imgs.search.brave.com/UwqyTvQWl82NmIKWCMUsQYa9xGcYrWizMJeFlvH-PcU/rs:fit:770:514:1/g:ce/aHR0cHM6Ly93d3cu/ZmxpcHBpbmdoZWNr/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNy9ob3ct/dG8tZ2V0LW1vdGl2/YXRlZC5qcGc"
+                alt=""
+              ></img> 
+              Title: TRUNGQUANDEV
+            </li>
+            <li>abc</li>
+            <li>abc</li>
+            <li>abc</li>
+          </ul>
+
+          <footer>Footer</footer>
+        </div>
+        <div className="column">
+          <header> Brainstorm</header>
+          <ul>
+            <li>
+               <img
+                src="https://imgs.search.brave.com/UwqyTvQWl82NmIKWCMUsQYa9xGcYrWizMJeFlvH-PcU/rs:fit:770:514:1/g:ce/aHR0cHM6Ly93d3cu/ZmxpcHBpbmdoZWNr/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAyMC8wNy9ob3ct/dG8tZ2V0LW1vdGl2/YXRlZC5qcGc"
+                alt=""
+              ></img> 
+              Title: TRUNGQUANDEV
+            </li>
+            <li>abc</li>
+            <li>abc</li>
+            <li>abc</li>
+          </ul>
+
+          <footer>Footer</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
