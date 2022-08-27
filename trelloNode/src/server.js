c8e5ee0c383f0a297c0102279bfb9647813e7916
TuanMinh7979 <minhtuanto179@gmail.com>
2022-08-27 21:40:07 +0700

    stable

--- trelloNode/src/server.js
+++ trelloNode/src/server.js
@@ -1,26 +1,36 @@
-import express from "express";
-import { connectDB, getDB } from "./config/mongodb";
-import { BoardModel } from "./models/board.model";
+import express from 'express';
+import { connectDB, getDB } from './config/mongodb';
+import { BoardModel } from './models/board.model';
 
-import { apiV1 } from "./routes/v1";
-const hostname = "localhost";
+import { apiV1 } from './routes/v1';
+const hostname = 'localhost';
 const port = 8989;
 
 connectDB()
-  .then(() => console.log("Connected success to server"))
+  .then(() => console.log('Connected success to server'))
   .then(() => bootServer())
   .catch((e) => {
     console.log(e);
     process.exit();
   });
-const bootServer = () => { 
+const bootServer = () => {
   const app = express();
+
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
   app.use(express.json());
-  app.use('/v1', apiV1)
+  app.use('/v1', apiV1);
 
-
-  app.get("/", (req, res) => {
-    res.send("<h1>abc</h1>");
+  app.get('/', (req, res) => {
+    res.send('<h1>abc</h1>');
   });
 
   // app.get("/test", async (req, res) => {
@@ -35,6 +45,6 @@ const bootServer = () => {
   // });
 
   app.listen(port, hostname, () => {
-    console.log("hello trung quna dev");
+    console.log('hello trung quna dev');
   });
 };
